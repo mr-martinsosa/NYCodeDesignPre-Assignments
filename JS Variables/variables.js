acdae8230daa907d8cf01a488a057de823274459
Martin <mr.martinsosa@gmail.com>
2018-01-09 20:20:43 -0500

    Uploaded 2 assignments

---
+++ JS Variables/variables.js
@@ -0,0 +1,36 @@
+/*Store the number 10 and the number 3 in two different variables.
+Multiply those two numbers together.*/
+var ten = 10;
+var three = 3;
+var thirty;
+
+thirty = ten * three;
+
+/*Store the number 25 and the number 5 in two different variables.
+Divide the variable holding the number 25 by the variable holding number 5.*/
+var twentyFive = 25;
+var five = 5;
+var fiveAnswer;
+
+fiveAnswer = twentyFive / five;
+
+/*Store your age into a variable.
+Calculate how many years you have left until you are 100 years old.*/
+var age = 25;
+var hundred = 100;
+var yearsLeft;
+
+yearsLeft = hundred - age;
+
+/*Store the number 27 and your age into two separate variables.
+Multiply the variable that stored the number 27 by 1337, subtract your
+age from this and reduce that amount by 509.*/
+var twentySeven = 27;
+var result;
+
+result = ((twentySeven * 1337) - age) - 509;
+
+/*Store the result of the above operation in a variable.
+Output the words “The result of my operation is = [result]” to the console.*/
+console.log("The result of my operation is = " + result);
+window.alert("The result of my operation is = " + result); //in case you don't see it
